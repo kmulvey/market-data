4f7f23ecd3cb716a7b84faa4f8eaf327fdae762b
Kevin Mulvey <kmulvey@shutterstock.com>
2014-04-16 19:01:21 -0400

    db stuff

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var request = require('request');
 var csv = require('csv');
+var mysql = require('mysql');
 
 var MA_PERIOD = 20;
 var ME_PERIOD_RES = 120;
@@ -7,6 +8,7 @@ var twenty_ma_arr = new Array();
 var twenty_ma = 0;
 var price_line_status = "";
 
+/*
 var tick_num = 0;
 setInterval(function(){
 	request('http://finance.yahoo.com/d/quotes.csv?s=GOOG&f=l1', function (error, response, body) {
@@ -48,15 +50,40 @@ setInterval(function(){
 		}
 	});
 }, 1000);
-
-/*
+*/
 request('http://ichart.finance.yahoo.com/table.csv?s=IVV&a=05&b=15&c=2000&d=04&e=15&f=2014&g=d&ignore=.csv', function (error, response, body) {
 	if (!error && response.statusCode == 200) {
 		csv()
 			.from.string(body)
 			.to.array( function(data){
-				console.log(data.length);
+				console.log(data);
 			});
 	}
 });
-*/
+
+
+
+function handleDisconnect() {
+  connection = mysql.createConnection(config); 
+
+  connection.connect(function(err) { 
+    if(err) {
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); 
+    } 
+		else{
+			console.log('connected to db');
+		}
+  });                                     
+  
+	connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect(); 
+    } else {
+      throw err; 
+    }
+  });
+}
+
+handleDisconnect();
