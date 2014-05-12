d45598c15f453f9d517a4bc0efc0138f0c75b987
Kevin Mulvey <king.feruke@gmail.com>
2014-05-12 07:32:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ request('http://ichart.finance.yahoo.com/table.csv?s=IVV&a=05&b=15&c=2000&d=04&e
   if (!error && response.statusCode == 200) {
     csv()
       .from.string(body)
-			.to.array( function(data){
+      .to.array( function(data){
 				console.log(data);
 			});
 	}
