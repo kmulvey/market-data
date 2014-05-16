4479f1ca57ef570e630e299c2d6ff5f7a6318f5a
Kevin Mulvey <king.feruke@gmail.com>
2014-05-16 17:57:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ var price_line_status = "";
 var tick_num = 0;
 setInterval(function(){
   request('http://finance.yahoo.com/d/quotes.csv?s=GOOG&f=l1', function (error, response, body) {
-		if (!error && response.statusCode == 200) {
+  if (!error && response.statusCode == 200) {
 			tick_num++;
 
 			var tick = parseFloat(body.trim());
