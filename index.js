a7a080477fa818207f544fe5ed46ca2b8b7e9b39
Kevin Mulvey <king.feruke@gmail.com>
2014-06-15 20:02:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -33,8 +33,8 @@ setInterval(function(){
     twenty_ma = sum/twenty_ma_arr.length;
   }
 	
-  if(twenty_ma_arr.length == MA_PERIOD){
-				if(tick > twenty_ma){
+  if(twenty_ma_arr.length == MA_PERIOD){ 
+    if(tick > twenty_ma){
 					if(price_line_status != "above") {
 						console.log("it crossed going up ... SELL & SHORT", twenty_ma, tick);
 						price_line_status = "above";
